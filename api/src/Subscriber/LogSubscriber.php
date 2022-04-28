b512c6ce2c7a5f5067e4b2697d3d971ef7eb3bdc
Barry Brands <barrybrands02@hotmail.com>
2022-04-28 14:31:04 +0200

    LogSubscriber added on TerminateEvent (not working yet)

--- api/src/Subscriber/LogSubscriber.php
+++ api/src/Subscriber/LogSubscriber.php
@@ -4,30 +4,30 @@ namespace App\Subscriber;
 
 use App\Service\LogService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\TerminalEvent;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class LogSubscriber implements EventSubscriberInterface
 {
-    private LogService $logService;
+  private LogService $logService;
 
-    public function __construct(LogService $logService)
-    {
-        $this->logService = $logService;
-    }
+  public function __construct(LogService $logService)
+  {
+    $this->logService = $logService;
+  }
 
-    public static function getSubscribedEvents()
-    {
-        return [
-            KernelEvents::RESPONSE => ['requestLog'],
-        ];
-    }
+  public static function getSubscribedEvents()
+  {
+    return [
+      KernelEvents::TERMINATE => ['requestLog'],
+    ];
+  }
 
-    public function requestLog(ResponseEvent $event)
-    {
-        $response = $event->getResponse();
-        $request = $event->getRequest();
+  public function requestLog(TerminateEvent $event)
+  {
+    $response = $event->getResponse();
 
-        // $this->logService->createLog($response, $request);
-    }
+    $this->logService->updateLogResponse($response, null, true, true);
+  }
 }
